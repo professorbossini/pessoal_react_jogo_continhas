7e0b14dd59167194566e241aa714572e6de22b95
Rodrigo Bossini <professorbossini@gmail.com>
2021-09-10 18:59:09 -0300

    novo index.js concluido ao construir o pdf

--- src/index.js
+++ src/index.js
@@ -9,8 +9,37 @@ import 'primeflex/primeflex.css';
 import Cartao from './Cartao';
 import Mensagem from './Mensagem';
 import Botoes from './Botoes';
+import Jogo from './Jogo';
+import GraficoLinha from './GraficoLinha';
 
 export default class App extends Component {
+  state = {
+    // jogo iniciado ou não?
+    status: 'off',
+    //número de acertos
+    acertos: 0,
+    //número de erros
+    erros: 0,
+    //número de tentativas
+    contador: 0
+  }
+  alterarStatus = (status) => {
+    this.setState({ status });
+  };
+  atualizarPontuacao = (acertou) => {
+    this.setState(
+      acertou
+        ? { acertos: this.state.acertos + 1, contador: this.state.contador + 1}
+        : { erros: this.state.erros + 1, contador: this.state.contador + 1}
+    );
+  };
+
+  zerarPontuacao = () => {
+    this.setState({
+      acertos: 0,
+      erros: 0,
+    });
+  };
     render() {
     return (
         //grid conteúdo centralizado horizontalmente
@@ -21,13 +50,25 @@ export default class App extends Component {
           <Cartao className='h-18rem'>
           {/* garantindo altura para alternar entre mensagem e jogo */}
             <div className='h-12rem'>
-              
-              {/* centraliza e pega toda a altura que pode */}
-                <div className='flex align-items-center h-full justify-content-center'>
-                  <Mensagem texto='Clique para iniciar' className='md:w-8 w-10' />
-                </div>
+              {this.state.status === 'on' ? (
+                  <Jogo
+                    status={this.state.status}
+                    fAtualizarPontuacao={this.atualizarPontuacao}
+                  />
+                ) : (
+                  <div className='flex align-items-center h-full justify-content-center'>
+                    <Mensagem texto='Clique para iniciar' className='w-6' />
+                  </div>
+                )}
             </div>
-            <Botoes />
+            <Botoes 
+              fIniciar={() => this.alterarStatus('on')}
+              fEncerrar={() => this.alterarStatus('off')}
+              fZerar={() => this.zerarPontuacao()}
+            />
+            {/* comentar */}
+            {/* {`Acertos: ${this.state.acertos}`}
+            {`Erros: ${this.state.erros}`} */}
           </Cartao>
         </div>
         {/* 12 colunas. 6 para telas grandes */}
@@ -36,6 +77,12 @@ export default class App extends Component {
             titulo="Sua pontuação"
             // altura igual à do outro
             className='h-18rem'>
+            <GraficoLinha 
+                  acertos={this.state.acertos}
+                  erros={this.state.erros}
+                  contador={this.state.contador}
+                  zerar={(!this.state.acertos && !this.state.erros)}
+                  />
           </Cartao>
         </div>
       </div>
